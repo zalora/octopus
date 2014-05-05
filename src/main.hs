4101470d98a9b4b7c99f4a906a7bd1f58bd6a650
Vladimir Kirillov <proger@wilab.org.ua>
2014-05-05 17:14:10 +0300

    add ekg, relax base version constraints up to <4.8

--- src/main.hs
+++ src/main.hs
@@ -3,5 +3,9 @@ module Main where
 import qualified Web.Scotty as S
 import Octopus.HTTP
 
+import qualified System.Remote.Monitoring as Mon
+
 main :: IO ()
-main = S.scotty 8000 scotty
+main = do
+    _ <- Mon.forkServer "localhost" 8001
+    S.scotty 8000 scotty
