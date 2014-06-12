16edaab9daaef6ccc55914935f19712d11e770ec
Simon Hengel <sol@typeful.net>
2014-06-12 21:40:02 +0800

    Check for "Content-Type: application/json" in tests

--- test/Helper.hs
+++ test/Helper.hs
@@ -3,13 +3,14 @@ module Helper where
 
 import           Test.Hspec.Wai
 import           Test.Hspec.Wai.Internal
+import           Network.HTTP.Types
 import           Network.Wai.Test
 import           Data.Aeson
 import           Data.Aeson.QQ
 import           Language.Haskell.TH.Quote
 
 shouldRespondWithJSON :: WaiSession SResponse -> Value -> WaiExpectation
-shouldRespondWithJSON r v = r `shouldRespondWith` ResponseMatcher 200 (Just $ encode v)
+shouldRespondWithJSON r v = r `shouldRespondWith` ResponseMatcher 200 [(hContentType, "application/json")] (Just $ encode v)
 
 json :: QuasiQuoter
 json = aesonQQ
